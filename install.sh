e28d664ead8287c52ab8d13dd81622289b57afe1
arslan <111767266+arslanben@users.noreply.github.com>
2024-08-06 23:39:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ REQUIRED_VERSION="3.6"
 version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
 
 if version_gt $REQUIRED_VERSION $CURRENT_PYTHON; then
-    echo "Python sürümünüz $CURRENT_PYTHON. Updating..."
+    echo "Python version $CURRENT_PYTHON. Updating..."
     sudo apt update
     sudo apt install -y python3 python3-pip
     NEW_PYTHON=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')
